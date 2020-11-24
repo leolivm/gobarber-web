a2e04a1854af215a7fa0022c45916d204d289bf8
Leandro Martins <leolivm@outlook.com>
2020-11-23 23:00:49 -0300

    second commit

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,8 +1,230 @@
-import React from "react";
-import { Container } from "./styles";
+import React, { useCallback, useEffect, useMemo, useState } from "react";
+import { isToday, format } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
+import DayPicker, { DayModifiers } from 'react-day-picker';
+import 'react-day-picker/lib/style.css';
+import {
+  Container,
+  HeaderContent,
+  Profile,
+  Header,
+  Content,
+  Schedule,
+  Calendar,
+  NextAppointment,
+  Section,
+  Appointment
+} from "./styles";
+import api from "../../services/api";
+import logoImg from "../../assets/logo.svg";
+import { FiClock, FiPower } from "react-icons/fi";
+import { useAuth } from "../../hooks/auth";
+
+interface MonthAvailabilityItem {
+  day: number;
+  available: boolean;
+}
+
+interface Appointment {
+  id: string,
+  date: string,
+  user: {
+    name: string,
+    avatar_url: string,
+  }
+}
 
 const Dashboard: React.FC = () => {
-  return <Container />;
+  const [selectedDate, setSelectedDate] = useState(new Date());
+  const [currentMonth, setCurrentMonth] = useState(new Date());
+  const [appointments, setAppointments] = useState<Appointment[]>([]);
+  const { signOut, user } = useAuth();
+  const [monthAvailability, setMonthAvailability] = useState<MonthAvailabilityItem[]>([]);
+
+  const handleDateChange = useCallback((day: Date, modifiers: DayModifiers) => {
+    if (modifiers.available) {
+      setSelectedDate(day);
+    }
+  }, []);
+
+  const handleMonthChange = useCallback((month: Date) => {
+    setCurrentMonth(month);
+  }, []);
+
+  useEffect(() => {
+    api.get(`/providers/${user.id}/month-availability`, {
+      params: {
+        year: currentMonth.getFullYear(),
+        month: currentMonth.getMonth() + 1,
+      }
+    }).then((response) => {
+      setMonthAvailability(response.data);
+    });
+  }, [currentMonth, user.id]);
+
+  useEffect(() => {
+    api.get("/appointments/me", {
+      params: {
+        year: selectedDate.getFullYear(),
+        month: selectedDate.getMonth() + 1,
+        day: selectedDate.getDate(),
+      }
+    }).then((response) => {
+      setAppointments(response.data);
+      console.log(response.data);
+    });
+  }, [selectedDate]);
+
+  const disabledDays = useMemo(() => {
+    const dates = monthAvailability.filter(monthDay => monthDay.available === false).map((monthDay) => {
+      const year = currentMonth.getFullYear();
+      const month = currentMonth.getMonth();
+      return new Date(year, month, monthDay.day);
+    })
+    return dates;
+  }, [currentMonth, monthAvailability]);
+
+  const selectedDateAsText = useMemo(() => {
+    return format(selectedDate, "'Dia' dd 'de' MMMM", {
+      locale: ptBR
+    });
+  }, [selectedDate]);
+
+  const selectedWeekDay = useMemo(() => {
+    return format(selectedDate, "cccc", { locale: ptBR });
+  }, [selectedDate]);
+
+  return (
+    <Container>
+      <Header>
+        <HeaderContent>
+          <img src={logoImg} alt={logoImg} />
+
+          <Profile>
+            <img src={user.avatar_url} alt={user.name} />
+
+            <div>
+              <span>Bem-vindo,</span>
+              <strong>{user.name}</strong>
+            </div>
+          </Profile>
+
+          <button type="button" onClick={signOut}>
+            <FiPower />
+          </button>
+        </HeaderContent>
+      </Header>
+
+      <Content>
+        <Schedule>
+          <h1>Horários agendados</h1>
+          <p>
+          {isToday(selectedDate) && <span>Hoje</span>}
+            <span>{selectedDateAsText}</span>
+            <span>{selectedWeekDay}</span>
+          </p>
+
+          <NextAppointment>
+            <strong>Atendimento a seguir</strong>
+            <div>
+              <img
+                src="https://avatars0.githubusercontent.com/u/59733804?s=460&u=78dd6fbf6e7d045a1278f11314c34ea0d8f82317&v=4"
+                alt="Leandro Martins"
+              />
+              <strong>Leandro Martins</strong>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+            </div>
+          </NextAppointment>
+
+          <Section>
+            <strong>Manhã</strong>
+
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+
+              <div>
+                <img
+                  src="https://avatars0.githubusercontent.com/u/59733804?s=460&u=78dd6fbf6e7d045a1278f11314c34ea0d8f82317&v=4"
+                  alt="Leandro Martins"
+                />
+                <strong>Leandro Martins</strong>
+              </div>
+            </Appointment>
+
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+
+              <div>
+                <img
+                  src="https://avatars0.githubusercontent.com/u/59733804?s=460&u=78dd6fbf6e7d045a1278f11314c34ea0d8f82317&v=4"
+                  alt="Leandro Martins"
+                />
+                <strong>Leandro Martins</strong>
+              </div>
+            </Appointment>
+          </Section>
+
+          <Section>
+            <strong>Tarde</strong>
+
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+
+              <div>
+                <img
+                  src="https://avatars0.githubusercontent.com/u/59733804?s=460&u=78dd6fbf6e7d045a1278f11314c34ea0d8f82317&v=4"
+                  alt="Leandro Martins"
+                />
+                <strong>Leandro Martins</strong>
+              </div>
+            </Appointment>
+          </Section>
+        </Schedule>
+
+        <Calendar>
+          <DayPicker
+            weekdaysShort={['D', 'S', 'T', 'Q', 'Q', 'S', 'S']}
+            fromMonth={new Date()}
+            disabledDays={[
+              { daysOfWeek: [0, 6] }, ...disabledDays
+            ]}
+            modifiers={{
+              available: { daysOfWeek: [1, 2, 3, 4, 5] }
+            }}
+            selectedDays={selectedDate}
+            onDayClick={handleDateChange}
+            onMonthChange={handleMonthChange}
+            months={[
+              'Janeiro',
+              'Fevereiro',
+              "Março",
+              "Abril",
+              "Maio",
+              "Junho",
+              "Julho",
+              "Agosto",
+              "Setembro",
+              "Outubro",
+              "Novembro",
+              "Dezembro"
+            ]}
+          />
+        </Calendar>
+      </Content>
+    </Container>
+  );
 };
 
 export default Dashboard;
--- src/pages/ForgotPassword/index.tsx
+++ src/pages/ForgotPassword/index.tsx
@@ -1,55 +1,51 @@
-import React, { useCallback, useRef, useState } from "react";
+import React, { useRef, useCallback, useState } from "react";
+import { FiLogIn, FiMail } from "react-icons/fi";
 import { Form } from "@unform/web";
-import * as Yup from "yup";
-import { Link, useHistory } from "react-router-dom";
-import { FiArrowLeft, FiMail, FiUser, FiLock } from "react-icons/fi";
+import { Link } from "react-router-dom";
 import { FormHandles } from "@unform/core";
-import getValidationErrors from "../../utils/getValidationErrors";
-import api from "../../services/api";
+import * as Yup from "yup";
 import { useToast } from "../../hooks/toast";
-import { Container, AnimationContainer, Content, Background } from "./styles";
+import api from "../../services/api";
+import { Container, Content, AnimationContainer, Background } from "./styles";
+import getValidationErrors from "../../utils/getValidationErrors";
 import logo from "../../assets/logo.svg";
 import Input from "../../components/Input";
 import Button from "../../components/Button";
 
-interface SignUpFormData {
-  name: string;
+interface ForgotPasswordFormData {
   email: string;
-  password: string;
 }
 
-const SignUp: React.FC = () => {
+const ForgotPassword: React.FC = () => {
+  const [loading, setLoading] = useState(false);
   const formRef = useRef<FormHandles>(null);
   const { addToast } = useToast();
-  const history = useHistory();
-  const [loading, setLoading] = useState(false);
 
   const handleSubmit = useCallback(
-    async (data: SignUpFormData) => {
+    async (data: ForgotPasswordFormData) => {
       try {
         setLoading(true);
         formRef.current?.setErrors({});
 
         const schema = Yup.object().shape({
-          name: Yup.string().required("Nome obrigatório."),
           email: Yup.string()
             .required("E-mail obrigatório.")
             .email("Digite um e-mail válido."),
-          password: Yup.string().min(6, "No mínimo 6 dígitos."),
         });
 
         await schema.validate(data, {
           abortEarly: false,
         });
 
-        await api.post("/users", data);
-
-        history.push("/");
+        await api.post("/password/forgot", {
+          email: data.email,
+        });
 
         addToast({
           type: "success",
-          title: "Cadastro realizado!",
-          description: "Você já pode fazer seu logon no GoBarber.",
+          title: "E-mail de recuperação enviado.",
+          description:
+            "Enviamos um e-mail para confirmar a recuperação de senha, cheque usa caixa de entrada.",
         });
       } catch (err) {
         if (err instanceof Yup.ValidationError) {
@@ -61,47 +57,43 @@ const SignUp: React.FC = () => {
 
         addToast({
           type: "error",
-          title: "Erro no cadastro.",
-          description: "Ocorreu um erro ao fazer cadastro, tente novamente.",
+          title: "Erro na recuperação de senha.",
+          description:
+            "Ocorreu um erro ao tentar realizar a recuperação de senha, tente novamente.",
         });
       } finally {
         setLoading(false);
       }
     },
-    [addToast, history]
+    [addToast]
   );
 
   return (
     <Container>
-      <Background />
-
       <Content>
         <AnimationContainer>
           <img src={logo} alt={logo} />
 
           <Form ref={formRef} onSubmit={handleSubmit}>
-            <h1>Faça seu cadastro</h1>
-            <Input name="name" icon={FiUser} placeholder="Nome completo" />
+            <h1>Recuperar senha</h1>
+
             <Input name="email" icon={FiMail} placeholder="E-mail" />
-            <Input
-              name="password"
-              icon={FiLock}
-              type="password"
-              placeholder="Senha"
-            />
-            <Button type="submit" loading={loading}>
-              Cadastrar
+
+            <Button loading={loading} type="submit">
+              Recuperar
             </Button>
           </Form>
 
           <Link to="/">
-            <FiArrowLeft />
-            Voltar para home
+            <FiLogIn />
+            Voltar ao login
           </Link>
         </AnimationContainer>
       </Content>
+
+      <Background />
     </Container>
   );
 };
 
-export default SignUp;
+export default ForgotPassword;
